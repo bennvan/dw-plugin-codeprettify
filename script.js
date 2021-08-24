330477b776fd7181a7e4231d295b0f5532a07c05
Ben <ben.vanmagill16@gmail.com>
2021-08-24 23:24:10 +1000

    add copy to clip button req. bs3 icon

--- script.js
+++ script.js
@@ -15,3 +15,30 @@
     }
 })();
 
+jQuery(function () {
+
+    jQuery( 'pre.prettyprint, pre.code' ).each( function() {
+
+        var $elmt = jQuery( this );
+
+        // Add button
+        //$btn = jQuery( '<div class="codeclipboard_button_container"><button></button></div>' );
+
+        $btn = jQuery('<div class="codeclipboard_button" title="Copy to clipboard"></div>');
+
+        $elmt.prepend($btn);
+
+        // Setup click behaviour
+        $btn.click(function() {
+            $elmt.fadeTo(100, 0.3);
+            var $txt = jQuery( '<textarea />' );
+            $txt.val($elmt.text()).css({ width: "1px", height: "1px" }).appendTo('body');
+            $txt.select();
+            document.execCommand('copy');
+            $txt.remove();
+            $elmt.fadeTo(600, 1);
+        });
+
+    });
+
+});
