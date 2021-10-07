4812578edee2a109f9100e2ecf2ce2f00174e825
Ben <ben.vanmagill16@gmail.com>
2021-10-07 17:58:42 +1100

    switch to vanilla js

--- script.js
+++ script.js
@@ -4,41 +4,56 @@
  * @author Satoshi Sahara <sahara.satoshi@gmail.com>
  */
 
-(function(){
-    function init(event){
+function initPrettyPrint(event){
+        // highlight the code
         prettyPrint();
-    }
-    if(window.addEventListener) {
-        window.addEventListener("load",init,false);
-    } else if(window.attachEvent) {
-        window.attachEvent("onload",init);
-    }
-})();
 
-jQuery(function () {
 
-    jQuery( 'pre.prettyprint, pre.code' ).each( function() {
+        document.querySelectorAll('pre.prettyprint, pre.code, pre.file').forEach(elmt => {
+            var btn = document.createElement("DIV");
+            btn.title = 'Copy to Clipboard';
+            btn.classList.add('codeclipboard_button');
+            var parent = elmt.parentElement;
+            var wrapper;
+            if (parent.className == 'code-toolbar'){
+                wrapper = true;
+            } else {
+                wrapper = false;
+            }
+            // If parent wrapper is present, append to that. 
+            if (wrapper) {
+                parent.appendChild(btn);
+            } else {
+                elmt.appendChild(btn);
+            }
+            
+            // Add the click function
+            btn.onclick = function() {
+                if (wrapper){
+                    parent.classList.add('copied');
+                    setTimeout(function(){
+                        parent.classList.remove('copied');
+                    }, 1000);
+                } else {
+                    elmt.classList.add('copied');
+                    setTimeout(function(){
+                        elmt.classList.remove('copied');
+                    }, 1000);
+                }
+                var txt = document.createElement('TEXTAREA');
+                txt.innerHTML = elmt.innerText;
+                elmt.appendChild(txt);
+                txt.select();
+                document.execCommand('copy');
+                elmt.removeChild(txt);
+            }
 
-        var $elmt = jQuery( this );
-
-        // Add button
-        //$btn = jQuery( '<div class="codeclipboard_button_container"><button></button></div>' );
-
-        $btn = jQuery('<div class="codeclipboard_button" title="Copy to clipboard"></div>');
-
-        $elmt.prepend($btn);
-
-        // Setup click behaviour
-        $btn.click(function() {
-            $elmt.fadeTo(100, 0.3);
-            var $txt = jQuery( '<textarea />' );
-            $txt.val($elmt.text()).css({ width: "1px", height: "1px" }).appendTo('body');
-            $txt.select();
-            document.execCommand('copy');
-            $txt.remove();
-            $elmt.fadeTo(600, 1);
         });
 
-    });
-
-});
+    };
+    
+    if(window.addEventListener) {
+        window.addEventListener("load",initPrettyPrint,false);
+    } else if(window.attachEvent) {
+        window.attachEvent("onload",initPrettyPrint);
+    }
